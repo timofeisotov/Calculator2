9d535ee9b8042cb1adf1b97674ab6b0bd44ed31b
TimSotovKVgazPC2 <timofeisotov@gmail.com>
2026-06-02 16:03:04 +0300

    calculatorscrptfirst

--- Assets/Scripts/CalculatorScript.cs
+++ Assets/Scripts/CalculatorScript.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CalculatorScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_InputField inputField1;
+    [SerializeField] private TMP_InputField inputField2;
+    [SerializeField] private TMP_Text resultText;
+    public void OnClickPlus()
     {
-        
+        if (TryGetNumbers(out int num1, out int num2))
+        {
+            float result = num1 + num2;
+            resultText.text = result.ToString(); 
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickMinus()
     {
-        
+        if(TryGetNumbers(out int num1, out int num2))
+        {
+            float result = num1 - num2;
+            resultText.text = result.ToString();
+        }
+    }
+
+    private bool TryGetNumbers(out int num1, out int num2) {
+        num1 = 0;
+        num2 = 0;
+        bool success = true;
+        if (int.TryParse(inputField1.text, out int num_1)) {
+            num1 = num_1;
+        } else {
+            success = false;
+        }
+        if (int.TryParse(inputField2.text, out int num_2)) {
+            num2 = num_2;
+        } else {
+            success = false;
+        }
+        return success;
+    }
+    public void OnClickMultiplication()
+    {
+        if(TryGetNumbers(out int num1, out int num2))
+        {
+            float result = num1 * num2;
+            resultText.text = result.ToString();
+        }
+    }
+    public void OnClickDivision()
+    {
+        if(TryGetNumbers(out int num1, out int num2))
+        {
+            float result = num1 / num2;
+            resultText.text = result.ToString();
+        }
     }
 }
